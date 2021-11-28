582a89199ba5e4323107a3837596d5a2468798ed
Ksuvot <ksuvot36@yandex.ru>
2021-11-28 19:08:20 +0300

    add img files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,83 @@
 'use strict';
 
 (() => {
-    // TODO
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
+
+  let ballRadius = 10;
+
+  const paddleHeight = 10;
+  const paddleWidth = 75;
+  let paddleX = (canvas.width-paddleWidth)/2;
+
+  let rightPressed = false;
+  let leftPressed = false;
+
+  let x = canvas.width/2;
+  let y = canvas.height-30;
+  let dx = 2;
+  let dy = -2;
+
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
+
+  function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+  }
+
+  function keyUpHandler(e) {
+      if(e.keyCode == 39) {
+          rightPressed = false;
+      }
+      else if(e.keyCode == 37) {
+          leftPressed = false;
+      }
+  }
+
+  function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+    x += dx;
+    y += dy;
+
+    if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+      dx = -dx;
+    }
+
+    if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+      dy = -dy;
+    }
+
+    if (rightPressed && paddleX < canvas.width-paddleWidth) {
+      paddleX += 7;
+    }
+    else if (leftPressed && paddleX > 0) {
+      paddleX -= 7;
+    }
+  }
+
+  setInterval(draw, 10);
+
 })();
